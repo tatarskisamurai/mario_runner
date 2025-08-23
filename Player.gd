b4ec9917a2bc5488b447fb5545250dba663e4a4c
Tylor <gromtank733@gmail.com>
2025-08-23 11:49:55 +0400

    fixed bugs

--- Player.gd
+++ Player.gd
@@ -4,8 +4,8 @@ signal startuem
 
 # Настройки
 export var speed = 200
-export var jump_force = -400
-export var gravity = 800
+export var jump_force = -500
+export var gravity = 600
 
 # Скорость
 var velocity = Vector2.ZERO
@@ -26,11 +26,14 @@ func _physics_process(delta):
 	# Горизонтальное движение
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
+		sprite.play()
 		emit_signal("startuem")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -speed
+		sprite.play()
 	else:
 		velocity.x = 0
+		sprite.stop()
 
 	# Прыжок
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
